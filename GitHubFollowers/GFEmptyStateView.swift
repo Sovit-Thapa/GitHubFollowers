9816c86a072ec01e8d786d279529e59db3dbcba6
Sovit-Thapa <thapasovit123@gmail.com>
2024-09-11 22:39:06 -0400

    Search bar, Loading View and Empty Stage View Added

---
+++ GitHubFollowers/GFEmptyStateView.swift
@@ -0,0 +1,52 @@
+//
+//  GFEmptyStateView.swift
+//  GitHubFollowers
+//
+//  Created by Sovit Thapa on 2024-09-11.
+//
+
+import UIKit
+
+class GFEmptyStateView: UIView {
+    
+    let messageLabel = GFTitleLabel(textAlignment: .center, fontSize: 28)
+    let logoImageView = UIImageView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(message: String){
+        super.init(frame: .zero)
+        messageLabel.text = message
+        configure()
+    }
+    
+    private func configure(){
+        addSubview(messageLabel)
+        addSubview(logoImageView)
+        
+        messageLabel.numberOfLines = 3
+        messageLabel.textColor = .secondaryLabel
+        
+        logoImageView.image = UIImage(named: "empty-state-logo")
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            messageLabel.centerYAnchor.constraint(equalTo: centerYAnchor, constant: -100 ),
+            messageLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 40),
+            messageLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -40),
+            messageLabel.heightAnchor.constraint(equalToConstant: 200),
+            
+            logoImageView.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 1.3),
+            logoImageView.heightAnchor.constraint(equalTo: widthAnchor, multiplier: 1.3),
+            logoImageView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 170),
+            logoImageView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: 40)
+        ])
+    }
+}
